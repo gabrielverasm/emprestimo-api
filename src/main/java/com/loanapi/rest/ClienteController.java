a9a6f38349807cd2fc6914ec1c71019d9dd85299
Gabriel <gabriel.miranda@dellead.com>
2021-03-03 12:31:58 -0300

    individualizando o salvamento e o retorno solicaitado.

--- src/main/java/com/loanapi/rest/ClienteController.java
+++ src/main/java/com/loanapi/rest/ClienteController.java
@@ -7,36 +7,38 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.loanapi.model.entity.Cliente;
-import com.loanapi.model.entity.dto.ClienteModalidadeDTO;
 import com.loanapi.model.repository.ClientRepository;
-import com.loanapi.service.EmprestimoService;
 
 @RestController
-@RequestMapping("/api/emprestimo")
+@RequestMapping("/api/cliente")
 @CrossOrigin("http://localhost:4200")
-public class EmprestimoController {
+public class ClienteController {
 
 	private final ClientRepository clienteRepository;
-	private final EmprestimoService service;
 
 	@Autowired
-	public EmprestimoController(ClientRepository clienteRepository, EmprestimoService service) {
+	public ClienteController(ClientRepository clienteRepository) {
 		this.clienteRepository = clienteRepository;
-		this.service = service;
 	}
 
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Cliente save(@RequestBody @Valid Cliente cliente) {
+		return clienteRepository.save(cliente);
+	}
+	
 	@GetMapping("{id}")
-	public ClienteModalidadeDTO consultarEmprestimo(@PathVariable Integer id) {
-		Cliente cliente = new Cliente();
-		cliente = clienteRepository.getOne(id);
-		return service.EmprestimoDisponível(cliente);
+	public Cliente buscarPorId(@PathVariable Integer id) {
+		return clienteRepository.findById(id).orElseThrow(() ->
+		new ResponseStatusException(HttpStatus.NOT_FOUND));
 	}
 
 }
